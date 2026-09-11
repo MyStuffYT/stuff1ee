d1353daa10d8ab294cfbfad762c3123b0e7a9462
MyStuffYT <mystuffyt@proton.me>
2026-09-11 22:09:56 +0100

    try to fix deployment by using repo nmame??

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/stuff1ee/',
   plugins: [svelte(), tailwindcss()],
 })
