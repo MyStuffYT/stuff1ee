faea619f420045a042a82dd1a20277fc495231d2
MyStuffYT <mystuffyt@proton.me>
2026-09-11 22:19:10 +0100

    fix for the custom domain instead....

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/stuff1ee/',
+  base: '/',
   plugins: [svelte(), tailwindcss()],
 })
